ed75d353fc48c701f80ac673fda942213d32a8b4
Anjana <adeegoda@wiley.com>
2024-02-18 13:41:27 +0530

    Added database connection and vote submission

--- PVB-Backend/models/index.js
+++ PVB-Backend/models/index.js
@@ -1,40 +1,43 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
+const { env } = require('process');
 let results;
 
-const client = new MongoClient(process.env.DB_URI);
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB_URI).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
 
-module.exports.connectPVBDatabaseNew = async () => {
-    try {
-        await client.connect();
-        await client.db("PVB-Eballots").command({ ping: 1 });
-        console.log("Successfully connected to PVB Database!");
-    } catch (error) {
-        console.log('DB Connection error');
-        await client.close();
-    }
-}
+module.exports = mongoose;
 
-module.exports.getAllBallots = async () => {
-    try {
-        console.log("Retrieving Data..");
-        results = await client.db("PVB-Eballots").collection('eballots').find().toArray();
-        console.log(results);
-        return results;
-    } catch (error) {
-        console.log('DB Connection error');
-    }
-}
+// module.exports.connectPVBDatabaseNew = async () => {
+//     try {
+//         await client.connect();
+//         await client.db("PVB-Eballots").command({ ping: 1 });
+//         console.log("Successfully connected to PVB Database!");
+//     } catch (error) {
+//         console.log('DB Connection error');
+//         await client.close();
+//     }
+// }
 
-module.exports.submitBallot = async (ballot) => {
-    try {
-        console.log("Submiting Ballot..");
-        await client.db("PVB-Eballots").collection('eballots').insertOne(ballot);
-        console.log("Ballot Submitted!");
-    } catch (error) {
-        console.log('DB Connection error');
-    }
-}
+// module.exports.getAllBallots = async () => {
+//     try {
+//         console.log("Retrieving Data..");
+//         results = await client.db("PVB-Eballots").collection('eballots').find().toArray();
+//         console.log(results);
+//         return results;
+//     } catch (error) {
+//         console.log('DB Connection error');
+//     }
+// }
+
+// module.exports.submitBallot = async (ballot) => {
+//     try {
+//         console.log("Submiting Ballot..");
+//         await client.db("PVB-Eballots").collection('eballots').insertOne(ballot);
+//         console.log("Ballot Submitted!");
+//     } catch (error) {
+//         console.log('DB Connection error');
+//     }
+// }
