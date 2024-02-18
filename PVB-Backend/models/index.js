5ef88aa2a964548a5bf4a01a78eac146b6bfb5d5
Anjana <adeegoda@wiley.com>
2024-02-18 14:17:37 +0530

    Added formating

--- PVB-Backend/models/index.js
+++ PVB-Backend/models/index.js
@@ -1,43 +1,8 @@
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
-const { env } = require('process');
-let results;
 
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB_URI).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
+mongoose.connect(process.env.DB_URI).then(() => console.log('PVB Database Connected')).catch(err => console.log(err));
 
 module.exports = mongoose;
-
-// module.exports.connectPVBDatabaseNew = async () => {
-//     try {
-//         await client.connect();
-//         await client.db("PVB-Eballots").command({ ping: 1 });
-//         console.log("Successfully connected to PVB Database!");
-//     } catch (error) {
-//         console.log('DB Connection error');
-//         await client.close();
-//     }
-// }
-
-// module.exports.getAllBallots = async () => {
-//     try {
-//         console.log("Retrieving Data..");
-//         results = await client.db("PVB-Eballots").collection('eballots').find().toArray();
-//         console.log(results);
-//         return results;
-//     } catch (error) {
-//         console.log('DB Connection error');
-//     }
-// }
-
-// module.exports.submitBallot = async (ballot) => {
-//     try {
-//         console.log("Submiting Ballot..");
-//         await client.db("PVB-Eballots").collection('eballots').insertOne(ballot);
-//         console.log("Ballot Submitted!");
-//     } catch (error) {
-//         console.log('DB Connection error');
-//     }
-// }
