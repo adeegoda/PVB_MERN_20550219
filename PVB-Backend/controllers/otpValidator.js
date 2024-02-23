db716b961776c89c4501005f6bacf37aeff730ad
Anjana <adeegoda@wiley.com>
2024-02-23 16:59:51 +0530

    Added OTP verification Serverside

--- PVB-Backend/controllers/otpValidator.js
+++ PVB-Backend/controllers/otpValidator.js
@@ -4,16 +4,21 @@ async function validateOTP(req, res) {
     const enteredOTP = req.body.otp;
     try {
         const savedOTP = await OTP.findOne({ otp: enteredOTP });
-
+        if (!savedOTP) {
+            return res.status(404).json({ message: 'කල් ඉකුත් වූ OTP යකි | Expired OTP |காலாவதியான OTP' });
+        }
         if (savedOTP && savedOTP.otp === enteredOTP) {
-            res.json({ verified: true, message: 'OTP is valid' });
+            res.status(202).json({ verified: true, message: 'OTP is valid' });
+            console.log("Removing validated OTP..");
+            await OTP.deleteOne({ _id: savedOTP._id });
+            console.log("Removed validated OTP: " + savedOTP);
         } else {
-            res.json({ verified: false, message: 'OTP is not valid' });
+            res.json({ verified: false, message: 'OTP වලංගු නැත | OTP is not valid | OTP செல்லுபடியாகாது' });
         }
     } catch (err) {
         console.error('Error verifying OTP:', err);
         res.status(500).json({ error: 'Failed to verify OTP' });
     }
-} 
+}
 
 module.exports = { validateOTP };
