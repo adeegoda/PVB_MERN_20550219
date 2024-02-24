aa64db9a4984c84dcbf404a7c23fe4254621ab87
Anjana <adeegoda@wiley.com>
2024-02-24 22:15:52 +0530

    added separate loader for party details from the database

--- PVB-Backend/party-handlers/cardHandler.js
+++ PVB-Backend/party-handlers/cardHandler.js
@@ -1,11 +1,13 @@
-const cards = require('./party-cards');
+const Card = require('../models/cards');
 
 async function loadPartyDetails(req, res) {
     try {
+        const cards = await Card.find();
         res.status(200).json(cards);
         console.log('Part Cards Released Successfully!')
     } catch (error) {
-        res.status(500).json({ message: 'Failed to load Party Details' });
+        console.error('Error fetching card data:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 }
 
