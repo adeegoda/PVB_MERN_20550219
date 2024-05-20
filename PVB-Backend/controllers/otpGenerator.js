385d68861ba6487851ad134f4353c4ea950f327c
adeegoda <anjideegoda@gmail.com>
2024-05-20 11:43:17 +0530

    vivaFeedbackImplementation_Backend: Added capturing the Farud attepts

--- PVB-Backend/controllers/otpGenerator.js
+++ PVB-Backend/controllers/otpGenerator.js
@@ -1,12 +1,17 @@
 const { otpGen } = require('otp-gen-agent');
 const OTP = require('../models/otp');
 const OTP_NIC_MAP = require('../models/otp-nic-map');
+const OTP_NIC_ERROR = require('../models/otp-nic-map-errors');
 
 async function generateOTP(req, res) {
     const enteredNIC = req.body.nic;
     const nicRecord = await OTP_NIC_MAP.findOne({ nic: enteredNIC });
+    console.log('Entered NIC: ' + enteredNIC)
     try {
         if (nicRecord) {
+            const fraudAttempt = new OTP_NIC_ERROR({ nic: enteredNIC })
+            await fraudAttempt.save();
+            console.log('Farud Attempt Recorded')
             return res.status(401).json({ message: 'OTP අංකයකට සම්බන්ද NIC අංකයකි | OTP already existing for the NIC number | NIC எண்ணுக்கு ஏற்கனவே OTP உள்ளது' });
         }
         if (!nicRecord) {
